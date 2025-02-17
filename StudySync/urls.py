9878dafcbc57b840fbcaaaffd833443d39dd7e93
vidhisati19 <vsati@ualberta.ca>
2025-02-17 10:20:51 -0700

    Update urls.py

--- StudySync/urls.py
+++ StudySync/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+from accounts.views import dashboard
+
+# Redirect root URL to the login page
+def home(request):
+    return redirect('/accounts/login/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),  
+    path('accounts/', include('allauth.urls')),
+    path('', home, name='home'),
 ]
+
+urlpatterns += [
+    path('dashboard/', dashboard, name='dashboard'),
